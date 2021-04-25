6366252eef8e88ff2f848fc9faa61e3ec9ef4c08
Neil Gierman <neil.gierman@hpe.com>
2021-04-24 19:13:31 -0500

    First attempt GitHub Actions

--- stress-test.sh
+++ stress-test.sh
@@ -4,12 +4,12 @@ apk update
 apk add curl
 wget https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
 chmod +x wait-for-it.sh
-echo "Waiting for services to be up"
+echo "1. Waiting for services to be up"
 ./wait-for-it.sh rabbitmq:5672 --timeout=60
 ./wait-for-it.sh mongodb:27017 --timeout=60
 ./wait-for-it.sh app1:80 --timeout=60
 ./wait-for-it.sh app2:80 --timeout=60
-echo "Both of these should return unknown"
+echo "2. Both of these should return unknown"
 if [ "`curl -s http://app1/domains/example.com`" != "unknown" ]
 then
    exit 1
@@ -26,7 +26,8 @@ do
    curl -s -X PUT http://app1/events/example2.com/delivered &
 done
 wait $(jobs -p)
-echo "Both of these should still return unknown because we wrote 998 deliveries to example.com and example2.com"
+sleep 1
+echo "3. Both of these should still return unknown because we wrote 998 deliveries to example.com and example2.com"
 if [ "`curl -s http://app1/domains/example.com`" != "unknown" ]
 then
    exit 1
@@ -38,7 +39,8 @@ fi
 curl -s -X PUT http://app1/events/example.com/delivered &
 curl -s -X PUT http://app2/events/example.com/delivered &
 wait $(jobs -p)
-echo "Now the first should be catch-all and the second still unknown"
+sleep 1
+echo "4. Now the first should be catch-all and the second still unknown"
 if [ "`curl -s http://app1/domains/example.com`" != "catch-all" ]
 then
    exit 1
@@ -48,7 +50,8 @@ then
    exit 1
 fi
 curl -s -X PUT http://app1/events/example.com/bounced
-echo "Now the first should be not catch-all and the second still unknown"
+sleep 1
+echo "5. Now the first should be not catch-all and the second still unknown"
 if [ "`curl -s http://app1/domains/example.com`" != "not catch-all" ]
 then
    exit 1
